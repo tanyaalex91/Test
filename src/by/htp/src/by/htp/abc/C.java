758b7ddd19f9ada53282d7298d343e2610714aa8
Tatsiana <36223017+tanyaalex91@users.noreply.github.com>
2018-02-21 21:08:03 +0300

    Add files via upload

---
+++ src/by/htp/src/by/htp/abc/C.java
@@ -0,0 +1,23 @@
+package by.htp.abc;
+
+public class C extends B {
+
+	{
+		System.out.println("CL1");
+//new C ();
+	}
+
+	public C() {
+		System.out.println("C ()");
+	}
+
+	public C(String c) {
+		super();
+		System.out.println("C (String c)");
+	}
+
+	
+	public C (Object obj) {
+		this ("a");
+	}
+}
