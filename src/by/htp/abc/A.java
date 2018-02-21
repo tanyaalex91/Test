d28b14be42318073e309bf031679514163a4bb66
Tatsiana <36223017+tanyaalex91@users.noreply.github.com>
2018-02-21 21:06:50 +0300

    Add files via upload

---
+++ src/by/htp/abc/A.java
@@ -0,0 +1,29 @@
+package by.htp.abc;
+
+public class A {
+	static {
+		System.out.println("AS1");
+	}
+
+
+	
+	public A() {
+		this ("a");
+		
+		System.out.println("A ()");
+	}
+	
+	public A (String str) {
+		System.out.println("A" + "(str)");
+	}
+	
+	public A (int a) {
+		this();
+		System.out.println("A" +  "(int)");
+	}
+	
+	{
+		System.out.println("AL1");
+	}
+	
+}
---
+++ src/by/htp/lesson9/MainApp.java
@@ -0,0 +1,20 @@
+package by.htp.lesson9;
+
+import by.htp.abc.*;
+
+public class MainApp {
+
+	static {
+//		new C();
+		System.out.println("MainApp static");
+	}
+
+	public static void main(String[] args) {
+
+//		new C(new B());
+
+		new C ();
+		new C ();
+	}
+
+}
